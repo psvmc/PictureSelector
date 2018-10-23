ab01bb154fd5b44d1c4da69b816e68a72e4d373c
psvmc <183518918@qq.com>
2018-10-23 14:28:50 +0800

    v2.2.6

--- app/src/main/java/com/luck/pictureselector/App.java
+++ app/src/main/java/com/luck/pictureselector/App.java
@@ -2,9 +2,6 @@ package com.luck.pictureselector;
 
 import android.app.Application;
 
-import com.squareup.leakcanary.LeakCanary;
-
-
 /**
  * author：luck
  * project：PictureSelector
@@ -17,12 +14,5 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
-        LeakCanary.install(this);
     }
 }
